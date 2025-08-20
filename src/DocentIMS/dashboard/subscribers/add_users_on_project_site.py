e76ce428ce0a2336f602d64ea6f509495b2f5d22
Espen Moe-Nilssen <espen@medialog.no>
2025-08-20 16:58:26 +0200

    code for adding images to users on team site

--- src/DocentIMS/dashboard/subscribers/add_users_on_project_site.py
+++ src/DocentIMS/dashboard/subscribers/add_users_on_project_site.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import requests
 from plone import api
+from io import BytesIO
 
 
 def handler(obj, event):
@@ -43,9 +44,24 @@ def handler(obj, event):
             
             
             response = requests.post(users_endpoint, auth=auth, headers=headers, json=payload)
+            
 
             if response.status_code in (200, 201):  # 201 = created
                 print(f"✅ User {email} created")
+                # Upload portrait if exists
+                portrait = user.getProperty("portrait")  # usually this is a binary image
+                if portrait:
+                    portrait_bytes = portrait.data       # the binary image
+                    mime_type = portrait.contentType     # e.g., "image/png" or "image/jpeg"
+                    filename = portrait.filename or "portrait"
+                    portrait_endpoint = f"{users_endpoint}/{username}/@portrait"
+                    portrait_headers = {"Accept": "application/json"}  # no Content-Type for files
+                    files = {"file": (filename, BytesIO(portrait_bytes), mime_type)}
+                    r = requests.put(portrait_endpoint, auth=auth, headers=portrait_headers, files=files)
+                    if r.status_code == 204:
+                        print(f"✅ Portrait for '{userid}' uploaded successfully")
+                    else:
+                        print(f"⚠️ Failed to upload portrait for '{userid}'")
             elif response.status_code == 409:
                 print(f"⚠️ User {email} already exists")
             else:
