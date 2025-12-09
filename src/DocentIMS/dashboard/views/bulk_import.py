bc836ba6f75b8884bbc72e977ee78db0fc7b8ac0
Espen Moe-Nilssen <espen@medialog.no>
2025-12-09 17:10:29 +0100

    update username

--- src/DocentIMS/dashboard/views/bulk_import.py
+++ src/DocentIMS/dashboard/views/bulk_import.py
@@ -181,7 +181,7 @@ class BulkImport(form.Form):
             if missing:
                 continue
 
-            username = email.lower().strip()
+            username = row.get("user_name"),
             if api.user.get(username=username):
                 continue
 
